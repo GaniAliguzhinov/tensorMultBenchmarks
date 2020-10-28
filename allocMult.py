1aff653cc507012f0ef44a35e592712a1945de81
Gani Aliguzhinov <aliguzhinov.gani@gmail.com>
2020-10-28 07:23:29 +0600

    renamed files

---
+++ allocMult.py
@@ -0,0 +1,45 @@
+import numpy
+import timeit
+import matplotlib.pyplot as plt
+import sys
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import tensorflow as tf
+
+
+"""
+https://sanjayasubedi.com.np/python/efficient-matrix-multiplication-in-python/
+"""
+global A, B
+
+global n
+
+def allocate(n):
+    global A
+    global B
+    A = numpy.random.rand(n, n).astype(numpy.float32)
+    B = numpy.random.rand(n, n).astype(numpy.float32)
+
+def matmul1(a, b):
+    n = a.shape[0]
+    res = numpy.empty(a.shape)
+    for i in range(n):
+        for j in range(n):
+            res[i,j] = numpy.dot(a[i,:], b[:,j])
+    return res
+
+def matmul2(a,b):
+    res = numpy.empty(a.shape)
+    n = a.shape[0]
+    for i in range(n):
+        res[i] = numpy.dot(a[i], b)
+
+def matmul3(a,b):
+    return tf.matmul(a,b)
+
+for i in range(100):
+    n = 100+i*10
+    timer = timeit.Timer("allocate(n)", "import numpy; from __main__ import allocate, n")
+    t = 1000.0*timer.repeat(1,1)[0]
+    timer = timeit.Timer("matmul3(A, B)", "import tensorflow as tf; from __main__ import A,B,matmul3")
+    print("{:.3f} ".format(t+1000.0*numpy.min(timer.repeat(10, 1))), end='')
---
+++ mult.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <Eigen/Dense>
+#include <chrono>
+#include <iomanip>
+#include <vector>
+
+using namespace Eigen;
+using namespace std;
+
+double timeProduct(int n) {
+
+    double min = 10000000000;
+    MatrixXf a = MatrixXf::Random(n, n);
+    MatrixXf b = MatrixXf::Random(n, n);
+    for (int i = 0; i < 10; ++i) {
+        auto t2 = chrono::high_resolution_clock::now();
+        b = a*b;
+        auto t3 = chrono::high_resolution_clock::now();
+        double duration = chrono::duration_cast<chrono::microseconds>(t3-t2).count()/1000.0;
+        if (duration < min)
+            min = duration;
+    }
+    cout << b(0,0) << " ";
+    return min;
+}
+int main() {
+
+    // auto t1 = chrono::high_resolution_clock::now();
+    // int n = 10000;
+    // MatrixXf a = MatrixXf::Random(n, n);
+    // MatrixXf b = MatrixXf::Random(n, n);
+    // MatrixXf c = MatrixXf::Random(n, n);
+    // a = a*1;
+    // b = b*1;
+    // c = c*1;
+
+    // auto t2 = chrono::high_resolution_clock::now();
+    // c = a*b;
+    // auto t3 = chrono::high_resolution_clock::now();
+    // auto durationAll = chrono::duration_cast<chrono::microseconds>(t2-t1).count()/1000.0;
+    // auto duration = chrono::duration_cast<chrono::microseconds>(t3-t2).count()/1000.0;
+    // cout << "n = " << n << "\n";
+    // cout << "Allocation time:\n\t";
+    // cout << durationAll << " ms\n";
+    // cout << "Product time:\n\t";
+    // cout << duration << "ms\n";
+
+    vector<int> sizes;
+    vector<double> times;
+    for (int i = 0; i < 100; ++i) {
+        sizes.push_back(100 + i*10);
+        times.push_back(timeProduct(100+i*10));
+    }
+    for (auto t : times)
+        cout << t << " ";
+    cout << endl;
+}
